dafbb07c4bfb87c9f64640914d623296302cf0d7
Mario Bitter <m.bitter@icloud.com>
2022-09-20 20:51:37 +0200

    Initial Commit

---
+++ XCode/XCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeApp.swift
+//  XCode
+//
+//  Created by Mario Bitter on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct XCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
