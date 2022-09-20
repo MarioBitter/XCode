989858d718ebf5befbdddd0733102b733675926e
MarioBitter <114021306+MarioBitter@users.noreply.github.com>
2022-09-20 21:10:56 +0200

    Update ContentView.swift
    modify text to world

--- XCode/ContentView.swift
+++ XCode/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Mario!")
+            Text("Hello, World!")
         }
         .padding()
     }
